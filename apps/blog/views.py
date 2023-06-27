a1871cc8e66018f68a5c02fdbd143a9999316cc2
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-06-27 10:51:09 +0300

    Add Views and URLs for Post Detail & Post List (#4)

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    context = {'posts': posts}
+    return render(request, 'blog/post/list.html', context)
+
+
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
+
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context)
