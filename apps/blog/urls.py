516ea70adde3dbe19e171209f841665b673b7d42
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-06-30 16:05:42 +0300

    Add Post Recommendation Feature (#10)

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail, name='post_detail'
     ),
+    path('<uuid:post_id>/share/', views.post_share, name='post_share'),
 ]
