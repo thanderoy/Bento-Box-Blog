20d6c24e93936ceb985304b5ded2a6e2e602cd25
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-06-30 11:13:42 +0300

    Add Basic Pagination (#9)

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,17 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator
+# from django.views.generic import ListView
+
 
 from .models import Post
+
+
+# class PostListView(ListView):
+#
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -9,7 +19,15 @@ def post_list(request):
 
     paginator = Paginator(posts_raw, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If non-integer values, render first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If no next page, render last page.
+        posts = paginator.page(paginator.num_pages)
 
     context = {'posts': posts}
     return render(request, 'blog/post/list.html', context)
