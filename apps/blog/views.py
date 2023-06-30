a4d276dec9b53889b955b7891dc086da04e9d937
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-06-30 10:08:14 +0300

    Add Basic Pagination (#8)

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_raw = Post.published.all()
+
+    paginator = Paginator(posts_raw, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
 
     context = {'posts': posts}
     return render(request, 'blog/post/list.html', context)
