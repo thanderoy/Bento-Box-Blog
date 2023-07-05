3864a7c94cdac8c1108e1aad41820c102028ac9b
Roy Thande <roy.thande@gmail.com>
2023-07-05 17:08:45 +0300

    Add Query Weighting

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchQuery, SearchRank, SearchVector)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -141,9 +142,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data.get("query")
+            search_vector = SearchVector("title", weight="A") \
+                + SearchVector("body", weight="B")
+
+            search_query = SearchQuery(query)
+
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by("-rank")
 
     context = {
         "form": form,
