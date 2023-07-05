c5114cf3ab681b9cac4fad3e31c4c5fc026160ff
Roy Thande <roy.thande@gmail.com>
2023-07-05 14:25:45 +0300

    Add Full-text Search functionality

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -6,7 +7,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 
 SUBJECT_PREFIX = '[CLASSIC_BLOG] '
@@ -129,3 +130,25 @@ def post_comment(request, post_id):
         'comment': comment
     }
     return render(request, 'blog/post/comment.html', context)
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data.get("query")
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+
+    context = {
+        "form": form,
+        "query": query,
+        "results": results
+    }
+
+    return render(request, 'blog/post/search.html', context)
